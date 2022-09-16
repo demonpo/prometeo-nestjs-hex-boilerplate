dacf6d3b9d2cec35d0509e3e5b31ebc3673001be
Daniel Garcia <demonpogp@gmail.com>
2022-09-16 12:25:48 -0500

    Added error-handler-library- and jwt-handler-library

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { errorHandler } from '@prometeo-dev/error-handler-library/dist/middlewares';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(errorHandler());
   await app.listen(3000);
 }
 bootstrap();
