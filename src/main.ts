b6c279984dea7a8571768fbbc059c4c9db297d9a
Daniel Garcia <demonpogp@gmail.com>
2022-09-23 22:56:32 -0500

    Added ExceptionHandlerFilter and updated error-handler and jwt-handler libaries

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { errorHandler } from '@prometeo-dev/error-handler-library/dist/middlewares';
+import { ExceptionHandlerFilter } from './shared/infra/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(errorHandler());
+  app.useGlobalFilters(new ExceptionHandlerFilter());
   await app.listen(3000);
 }
 bootstrap();
